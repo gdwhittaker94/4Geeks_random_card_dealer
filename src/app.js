36b7a8bf2dacccf368f943a8a5394b9e239369a2
Guy David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-11-28 18:43:35 +0100

    Card history display: values not displaying

--- src/app.js
+++ src/app.js
@@ -2,22 +2,28 @@ function log(input) {
     console.log(input);
 }
 
-// Variables
+// HTML Elements
 const cardSets = ["♦", "♥", "♠", "♣"];
 const cardNumbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 const number = document.getElementById("number");
-const symbolDiv = document.getElementsByClassName("card__symbol")
+const symbolDiv = document.getElementsByClassName("card__symbol");
+const button = document.getElementById("btn");
+const cardHistoryDisplay = document.getElementById("cardHistoryContainer");
+
+// Variables
+let producedNumber = null;
+let producedSymbol = null;
+let historyArray = [];
 
 // const symbolTop = document.getElementById("symbolTop");
 // const symbolBottom = document.getElementById("symbolBottom");
-const button = document.getElementById("btn");
 
-// Random Array Item
+// Produce Random Array Item
 function randomItem(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-// Apply Random Items to HTML Elements
+// Produce Random Symbol
 function randomSymbol() {
     let randomSymbol = randomItem(cardSets);
 
@@ -32,17 +38,47 @@ function randomSymbol() {
             symbolDiv[i].textContent = randomSymbol;
         }
     }
+    producedSymbol = randomSymbol;
 }
 
+//* take number, take symbol, add them together, put combined value into an array, display array on page
+
+// Produce Random Number
 function randomNum() {
     let randomNumber = randomItem(cardNumbers);
     number.innerText = randomNumber;
+    producedNumber = randomNumber;
 }
 
-// Button Event Listeners
-button.addEventListener("click", randomSymbol);
-button.addEventListener("click", randomNum);
+// Combine Random Num + Symbol for Card History Array
+function cardCombination(number, symbol) {
+    return number + symbol
+}
 
-// On page Load Event
+// On Button Click (Combined function calls)
+button.addEventListener("click", () => {
+    randomSymbol();
+    randomNum();
+    let combinedCardValue = cardCombination(producedNumber, producedSymbol)
+    historyArray = [...historyArray, combinedCardValue]
+    updateHistoryDisplay();
+});
+
+function updateHistoryDisplay() {
+    cardHistoryDisplay.innerHTML = "";
+    // Create UL
+    const ul = document.createElement("ul");
+    // Iterate through historyArray and create li items
+    historyArray.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent(item);
+        ul.appendChild(li);
+    });
+    // Append ul to container
+    cardHistoryDisplay.appendChild(ul)
+
+}
+
+// On page Load
 window.addEventListener("load", randomSymbol);
 window.addEventListener("load", randomNum);
