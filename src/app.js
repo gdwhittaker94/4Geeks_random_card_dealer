cf58abc017add3bbdcbef8cda45149630128d7df
Guy David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-11-29 18:22:41 +0100

    Card history and color picker complete

--- src/app.js
+++ src/app.js
@@ -2,21 +2,22 @@ function log(input) {
     console.log(input);
 }
 
-// HTML Elements
+// HTML ELEMENTS ----------------------
 const cardSets = ["♦", "♥", "♠", "♣"];
 const cardNumbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 const number = document.getElementById("number");
 const symbolDiv = document.getElementsByClassName("card__symbol");
 const button = document.getElementById("btn");
 const cardHistoryDisplay = document.getElementById("cardHistoryContainer");
+const background = document.querySelector(".bg");
+const colorPicker = document.getElementById("colorPicker")
 
-// Variables
+// VARIABLES ----------------------
 let producedNumber = null;
 let producedSymbol = null;
 let historyArray = [];
 
-// const symbolTop = document.getElementById("symbolTop");
-// const symbolBottom = document.getElementById("symbolBottom");
+// FUNCTIONS ----------------------
 
 // Produce Random Array Item
 function randomItem(array) {
@@ -41,8 +42,6 @@ function randomSymbol() {
     producedSymbol = randomSymbol;
 }
 
-//* take number, take symbol, add them together, put combined value into an array, display array on page
-
 // Produce Random Number
 function randomNum() {
     let randomNumber = randomItem(cardNumbers);
@@ -52,8 +51,39 @@ function randomNum() {
 
 // Combine Random Num + Symbol for Card History Array
 function cardCombination(number, symbol) {
-    return number + symbol
+    return `${number} ${symbol}`
 }
+
+// Update Card History Container
+function updateHistoryDisplay() {
+    cardHistoryDisplay.innerHTML = "";
+    // Create History Box Header
+    const cardHistoryText = document.createElement("p");
+    cardHistoryText.textContent = "Card History";
+    // Create UL
+    const ul = document.createElement("ul");
+    // Iterate through historyArray and create li items
+    historyArray.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        // Turn text red if...
+        if (item.includes("♦") || item.includes("♥")) {
+            li.classList.add("card__symbol--red");
+        }
+        // Add li to ul
+        ul.appendChild(li);
+    });
+    // Append ul to container
+    cardHistoryDisplay.appendChild(cardHistoryText)
+    cardHistoryDisplay.appendChild(ul)
+}
+
+// Change Background color
+function changeColor(colorValue) {
+    background.style.backgroundColor = colorValue;
+}
+
+// EVENT LISTENERS ----------------------
 
 // On Button Click (Combined function calls)
 button.addEventListener("click", () => {
@@ -64,21 +94,11 @@ button.addEventListener("click", () => {
     updateHistoryDisplay();
 });
 
-function updateHistoryDisplay() {
-    cardHistoryDisplay.innerHTML = "";
-    // Create UL
-    const ul = document.createElement("ul");
-    // Iterate through historyArray and create li items
-    historyArray.forEach((item) => {
-        const li = document.createElement("li");
-        li.textContent(item);
-        ul.appendChild(li);
-    });
-    // Append ul to container
-    cardHistoryDisplay.appendChild(ul)
-
-}
-
 // On page Load
 window.addEventListener("load", randomSymbol);
 window.addEventListener("load", randomNum);
+
+// On Color Picker Change
+colorPicker.addEventListener("change", (e) => {
+    changeColor(e.target.value)
+})
